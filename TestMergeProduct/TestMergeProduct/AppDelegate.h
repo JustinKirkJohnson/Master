7a7a7f87e367f954f48926dec2e38f2bbaa3e00a
Justin <justin.kirk.johnson@gmail.com>
2012-09-25 13:14:49 -0600

    this is a test# Your branch is ahead of 'origin/master' by 1 commit.

---
+++ TestMergeProduct/TestMergeProduct/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  TestMergeProduct
+//
+//  Created by Johnson, Justin on 9/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ TestMergeProduct/TestMergeProduct/DetailViewController.h
@@ -0,0 +1,19 @@
+//
+//  DetailViewController.h
+//  TestMergeProduct
+//
+//  Created by Johnson, Justin on 9/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+// test
+
+@end
